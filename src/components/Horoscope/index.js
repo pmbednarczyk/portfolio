4fbb98e409bdeeb590d6aff091fb38f44870347c
Pawel <pavbed@gmail.com>
2018-07-05 19:47:40 +0200

    add basic structure

---
+++ src/components/Horoscope/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Horoscope = () => {
+	return (
+		<div className="horoscopeContainer" />
+	);
+};
+
+Horoscope.defaultProps = {};
+
+Horoscope.propTypes = {};
+
+export default Horoscope;
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,21 +1,16 @@
 import "../../theme/styles.scss";
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Horoscope from "../../components/Horoscope"
 
 document.addEventListener('DOMContentLoaded', () => {
 	class App extends React.Component {
-
-		dupa() {
-			const sraka = "heee 2";
-
-			return (
-				<p>{sraka}</p>
-		);
-		}
 		render() {
 			return (
-				<h1>Test :){this.dupa()}</h1>
+				<div>
+					<h1>My components:</h1>
+					<Horoscope />
+				</div>
 		);
 		}
 	}
