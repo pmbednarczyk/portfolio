fe1c151c7825bfa97dc466b6254cd85d97539b48
Pawel <pavbed@gmail.com>
2018-07-05 20:11:02 +0200

    prepare completed workshop branch

--- src/components/Horoscope/index.js
+++ src/components/Horoscope/index.js
@@ -1,16 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import data from '../../mockups/horoscope'
 
-const Horoscope = () => {
+
+const { currentDate, months } = data;
+const Horoscope = ({ title }) => {
+
+	const renderCurrentConstellation = () => {
+		const currentMonth = months.find(month => month.headline);
+
+		return (
+			<div className="currentConstellation">
+				<div className="imgContainer">
+					<img src={currentMonth.image} alt={currentMonth.constellation} className="img-fluid" />
+				</div>
+				<div className="contentContainer">
+					<h2>{currentMonth.constellation}</h2>
+					<span className="date">{currentMonth.dateRange}</span>
+					<p>{currentMonth.description}</p>
+				</div>
+			</div>
+		);
+	};
+
+	const renderMonths = () => {
+		if (!months.length) {
+			return null;
+		}
+
+		return (
+			<ul className="signsContainer">
+				{months.map(item => (
+					<li key={item.constellation}>
+						<div className="imgContainer">
+							<img src={item.image} alt={item.constellation} className="img-fluid" />
+						</div>
+						<h3>{item.constellation}</h3>
+						<span className="date">{item.dateRange}</span>
+						<p>{item.description}</p>
+					</li>))}
+			</ul>
+		);
+	};
+
 	return (
 		<div className="horoscopeContainer">
-			<h2>Horoscope</h2>
+			<div className="header">
+				<h2>{title}</h2>
+				<span>{currentDate}</span>
+			</div>
+			{renderCurrentConstellation()}
+			{renderMonths()}
 		</div>
 	);
 };
 
-Horoscope.defaultProps = {};
+Horoscope.defaultProps = {
+	title: '',
+};
 
-Horoscope.propTypes = {};
+Horoscope.propTypes = {
+	title: PropTypes.string,
+};
 
 export default Horoscope;
